76ab96544d982d90e66d9d5770d1599c467ed9cc
Kubix007 <kuba.dolinski@hotmail.com>
2022-01-19 00:14:57 +0100

    API Link Fix

--- src/api/Auth.js
+++ src/api/Auth.js
@@ -2,7 +2,7 @@ import React from 'react';
 import httpRequest from '../api/HttpRequest';
 import { Redirect } from 'react-router';
 
-const API = "http://localhost:8000/api";
+const API = "https://kindergarten-backend.herokuapp.com/api";
 
 const login = (email, password) => {
     if (IsLogged()) {
